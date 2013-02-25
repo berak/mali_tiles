dfc7ea4eaad4f2d136aea76409e978c500935739
berak <p4p4p4@web.de>
2013-02-25 10:12:28 +0100

    minor cleanup

---
+++ cascade_create.bat
@@ -0,0 +1,1 @@
+E:\code\opencv242\bin\release\opencv_createsamples.exe -info haar_create.txt -vec haar_vec.txt -bg haar_bg.txt -show 
---
+++ cascade_train.bat
@@ -0,0 +1,1 @@
+E:\code\opencv242\bin\Release\opencv_traincascade.exe -data data -vec haar_vec.txt -bg haar_bg.txt -numPos 31 -numNeg 52 -featureType LBP 
